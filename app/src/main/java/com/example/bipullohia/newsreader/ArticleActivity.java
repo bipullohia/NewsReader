2e935e4d031d44bf51d5083d91e2962a99f7ce68
Bipul Kumar Lohia <bipul.lohia@gmail.com>
2017-04-22 14:49:03 +0530

    firstrun, refresh added, removed content in offline. UI improvements

--- app/src/main/java/com/example/bipullohia/newsreader/ArticleActivity.java
+++ app/src/main/java/com/example/bipullohia/newsreader/ArticleActivity.java
@@ -1,25 +1,70 @@
 package com.example.bipullohia.newsreader;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class ArticleActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
 
-        WebView webView = (WebView) findViewById(R.id.webView);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new myWebClient());
 
         Intent intent = getIntent();
+        setTitle(intent.getStringExtra("title"));
 
-        webView.loadData(intent.getStringExtra("content"), "text/html", "UTF-8");
+        webView.loadUrl(intent.getStringExtra("urls"));
+
     }
+
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            progressBar.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
 }
+
+
+/*
+
+
+
+ */
--- app/src/main/java/com/example/bipullohia/newsreader/MainActivity.java
+++ app/src/main/java/com/example/bipullohia/newsreader/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bipullohia.newsreader;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
@@ -9,6 +10,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -28,16 +32,49 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> titles = new ArrayList<>();
-    ArrayList<String> content = new ArrayList<>();
+    ArrayList<String> urls = new ArrayList<>();
     ArrayAdapter arrayAdapter;
     SQLiteDatabase articlesDB;
+    SharedPreferences sharedPreferences = null;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuinflater = getMenuInflater();
+        menuinflater.inflate(R.menu.actionbar_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.refresh) {
+
+            DownloadInfo();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("Top 5 News");
+        sharedPreferences = getSharedPreferences("mySharedPref", MODE_PRIVATE);
+
+        if (sharedPreferences.getBoolean("firstRun", true)) {
+
+            DownloadInfo();
+            sharedPreferences.edit().putBoolean("firstRun", false).apply();
+
+        }
+
+
+        setTitle("Top News");
 
         ListView newsListView = (ListView) findViewById(R.id.newsListView);
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
@@ -48,17 +85,21 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
                 Intent intent = new Intent(getApplicationContext(), ArticleActivity.class);
-                intent.putExtra("content", content.get(i));
+                intent.putExtra("urls", urls.get(i));
+                intent.putExtra("title", titles.get(i));
                 startActivity(intent);
 
             }
         });
 
         articlesDB = openOrCreateDatabase("Articles", MODE_PRIVATE, null);
-
-        articlesDB.execSQL("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, articleId INTEGER, title VARCHAR, content VARCHAR)");
+        articlesDB.execSQL("CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, articleId INTEGER, title VARCHAR, urls VARCHAR)");
 
         updateListView();
+
+    }
+
+    private void DownloadInfo() {
 
         try {
             DownloadInfoTask task = new DownloadInfoTask();
@@ -75,24 +116,23 @@ public class MainActivity extends AppCompatActivity {
         Cursor c = articlesDB.rawQuery("SELECT * FROM articles", null);
 
         int titleindex = c.getColumnIndex("title");
-        int contentIndex = c.getColumnIndex("content");
+        int contentIndex = c.getColumnIndex("urls");
 
         if (c.moveToFirst()) {
 
             titles.clear();
-            content.clear();
+            urls.clear();
 
             do {
 
                 titles.add(c.getString(titleindex));
-                content.add(c.getString(contentIndex));
+                urls.add(c.getString(contentIndex));
 
             } while (c.moveToNext());
 
             arrayAdapter.notifyDataSetChanged();
 
         }
-
 
     }
 
@@ -142,9 +182,9 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONArray array = new JSONArray(result);
 
-                int maxnum = 5;
+                int maxnum = 20;
 
-                if (array.length() < 5) {
+                if (array.length() < 20) {
                     maxnum = array.length();
                 }
 
@@ -179,33 +219,33 @@ public class MainActivity extends AppCompatActivity {
                         String articleTitle = jo.getString("title");
                         String articleUrl = jo.getString("url");
 
-                        url = new URL(articleUrl);
-
-                        httpURLConnection = (HttpURLConnection) url.openConnection();
-                        in = httpURLConnection.getInputStream();
-
-                        reader = new InputStreamReader(in);
-                        data = reader.read();
-
-                        String articleContent = "";
-
-                        while (data != -1) {
-
-                            char current = (char) data;
-                            articleContent += current;
-
-                            data = reader.read();
-                        }
+//                        url = new URL(articleUrl);
+//
+//                        httpURLConnection = (HttpURLConnection) url.openConnection();
+//                        in = httpURLConnection.getInputStream();
+//
+//                        reader = new InputStreamReader(in);
+//                        data = reader.read();
+//
+//                        String articleContent = "";
+//
+//                        while (data != -1) {
+//
+//                            char current = (char) data;
+//                            articleContent += current;
+//
+//                            data = reader.read();
+//                        }
 
                         //Log.i("article content", articleContent);
 
-                        String sql = "INSERT INTO articles (articleId, title, content) VALUES (?, ?, ?)";
+                        String sql = "INSERT INTO articles (articleId, title, urls) VALUES (?, ?, ?)";
 
                         SQLiteStatement statement = articlesDB.compileStatement(sql);
 
                         statement.bindString(1, articleId);
                         statement.bindString(2, articleTitle);
-                        statement.bindString(3, articleContent);
+                        statement.bindString(3, articleUrl);
 
                         statement.execute();
 
